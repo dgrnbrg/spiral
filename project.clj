89da4c6c3f3bf8f9ecbf99960d96ccd5f90e501d
David Greenberg <dsg123456789@gmail.com>
2015-05-06 21:55:43 -0400

    Add clojars as default push target

--- project.clj
+++ project.clj
@@ -14,4 +14,5 @@
                                   [compojure "1.1.7"]
                                   [clj-time "0.4.4"]
                                   [http-kit "2.1.19"]
-                                  [org.immutant/web "2.0.0"]]}})
+                                  [org.immutant/web "2.0.0"]]
+                   :deploy-repositories  [["releases" :clojars]]}})
