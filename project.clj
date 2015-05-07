bfcbac04669820abbc2fbdd9d206baf7ae3baa84
David Greenberg <dsg123456789@gmail.com>
2015-05-06 21:55:48 -0400

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spiral "0.1.1-SNAPSHOT"
+(defproject spiral "0.1.1"
   :description "core.async based http server library"
   :url "http://github.com/dgrnbrg/spiral"
   :license {:name "Eclipse Public License"
